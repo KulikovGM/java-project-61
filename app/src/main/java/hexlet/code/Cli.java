2416c2e4578837504409f56e78ea584a8c2dc6a9
Grigoriy <kulikovgm@gmail.com>
2025-04-14 09:12:20 +0300

    Add Makefile and class Cli

---
+++ app/src/main/java/hexlet/code/Cli.java
@@ -0,0 +1,11 @@
+package hexlet.code;
+
+import java.util.Scanner;
+
+public class Cli {
+    public static void getByName() {
+        System.out.println("May I have your name?");
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Hello, " + scan.nextLine() + "!");
+    }
+}
