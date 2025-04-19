069b13f00fae920acb6995ebd139000ab8d965f3
Grigoriy <kulikovgm@gmail.com>
2025-04-19 09:45:17 +0300

    Add enter the game 1

--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -3,10 +3,25 @@ package hexlet.code;
 import java.util.Scanner;
 
 public class Cli {
+    public static int gameNumber;
+    public static String name;
+    public static void getNumber() {
+        System.out.println("Please enter the game number and press Enter.\n"
+                + "1 - Greet\n"
+                + "0 - Exit\n");
+        Scanner scan = new Scanner(System.in);
+        gameNumber = scan.nextInt();
+        System.out.println("Your choice: " + gameNumber);
+        if (gameNumber == 1) {
+            System.out.println("Welcome to the Brain Games!");
+            getByName();
+        }
+    }
+
     public static void getByName() {
         System.out.println("May I have your name?");
-        Scanner scan = new Scanner(System.in);
-        System.out.println("Hello, " + scan.nextLine() + "!");
-        scan.close();
+        Scanner scan1 = new Scanner(System.in);
+        name = scan1.nextLine();
+        System.out.println("Hello, " + name + "!");
     }
 }
