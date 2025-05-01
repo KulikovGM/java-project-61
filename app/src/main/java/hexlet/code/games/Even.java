518a9e1d7875f359b24dba843ae8042852d38f94
Grigoriy <kulikovgm@gmail.com>
2025-05-01 17:27:36 +0300

    Change Even game. Add logger 2.

--- app/src/main/java/hexlet/code/games/Even.java
+++ app/src/main/java/hexlet/code/games/Even.java
@@ -4,37 +4,48 @@ import hexlet.code.Cli;
 
 import java.security.SecureRandom;
 import java.util.Scanner;
+import java.util.logging.Logger;
 
 public class Even {
     private static final int NUMBER_OF_ROUNDS = 3;
     private static final int RANGE_NUMB = 30;
+
+    private Even() {
+    }
+
     public static void game() {
-        System.out.println("Answer 'yes' if the number is even, otherwise answer 'no'.");
+        Logger logger1 = Logger.getLogger(Even.class.getName());
+        logger1.info("Answer 'yes' if the number is even, otherwise answer 'no'.");
         for (int i = 0; i < NUMBER_OF_ROUNDS; i++) {
             SecureRandom operatorChoice = new SecureRandom();
             int number = operatorChoice.nextInt(RANGE_NUMB);
-            System.out.println("Question: " + number);
+            String question = "Question: " + number;
+            Logger logger = Logger.getLogger(Even.class.getName());
+            logger.info(question);
             Scanner scan1 = new Scanner(System.in);
             String answer = scan1.nextLine();
-            System.out.println("Your answer: " + answer);
+            String yourAnswer = "Your answer: " + answer;
+            logger.info(yourAnswer);
             if (number % 2 == 0) {
                 if (answer.equals("yes")) {
-                    System.out.println("Correct!");
+                    logger.info("Correct!");
                 } else {
-                    System.out.println("'" + answer + "' is wrong answer ;(. Correct answer was 'yes'.");
-                    System.out.println("Let's try again, " + Cli.getName() + "!");
+                    String wrongAnswer = "'" + answer + "' is wrong answer ;(. Correct answer was 'yes'.";
+                    logger.info(wrongAnswer);
+                    logger.info("Let's try again, " + Cli.getName() + "!");
                     return;
                 }
             } else {
                 if (answer.equals("no")) {
-                    System.out.println("Correct!");
+                    logger.info("Correct!");
                 } else {
-                    System.out.println("'" + answer + "' is wrong answer ;(. Correct answer was 'no'.");
-                    System.out.println("Let's try again, " + Cli.getName() + "!");
+                    String wrongAnswer = "'" + answer + "' is wrong answer ;(. Correct answer was 'no'.";
+                    logger.info(wrongAnswer);
+                    logger.info("Let's try again, " + Cli.getName() + "!");
                     return;
                 }
             }
         }
-        System.out.println("Congratulations, " + Cli.getName() + "!");
+        logger1.info("Congratulations, " + Cli.getName() + "!");
     }
 }
